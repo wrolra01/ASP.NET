f9a13d68e0a76cdf3f0e0aa2c1a1aa8c4cfcd4b9
Rachel Wrolstad <wrolstadr@students.westerntc.edu>
2021-01-25 20:57:39 -0600

    finished step 1 of Party Invites

---
+++ PartyInvites/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            int hour = DateTime.Now.Hour;
+            string greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            ViewBag.Greeting = greeting;
+
+            return View();
+            //return "Hello from the HomeControllers's Index Action!";
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(Models.Guest guest)
+        {
+            Repository.AddResponse(guest);
+            return View("Thanks", guest);
+        }
+
+        public ActionResult ListResponses() 
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/PartyInvites/PartyInvites/Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<Guest> responses = new List<Guest>();
+
+        public static IEnumerable<Guest> Responses 
+        {
+            get { return responses; }
+        }
+
+        public static void AddResponse(Guest guest) 
+        {
+            responses.Add(guest);
+        }
+    }
+}
